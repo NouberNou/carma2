f66a9319d4818b8a469923460a2de1b8269d39b1
Nou <cliff.foster@idi-systems.com>
2015-11-11 23:19:37 -0800

    SQF Library initial files.

---
+++ rv/addons/lib/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+	class CarmaLib {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = { "CBA_XEH" };
+		version = 1.0;
+		author[] = {"Nou"};
+	};
+};
+
+class Extended_PreInit_EventHandlers {
+	class CarmaLib {
+		init = "[] call (compile preprocessFileLineNumbers '\x\carma2\rv\addons\lib\XEH_pre_init.sqf');";
+	};
+};
