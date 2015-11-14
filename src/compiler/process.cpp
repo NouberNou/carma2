cc9cfaa7e910860a2893b36325d08b82bbf7a092
Nou <cliff.foster@idi-systems.com>
2015-11-14 01:51:50 -0800

    Fixes for arrays.

--- src/compiler/process.cpp
+++ src/compiler/process.cpp
@@ -3,22 +3,20 @@
 namespace carma {
 	namespace process {
 		std::string process_input(std::string input_) {
-			std::string output;
 			token_list tokens = tokenize(input_);
 
-			process_accessors(tokens);
+			process_accessors(tokens, tokens.begin());
 			process_simple_assigments(tokens, tokens.begin());
 			process_array_accessors(tokens, tokens.begin());
-			tokens = tokenize(build_string(tokens));
 			process_method_calls(tokens, tokens.begin());
+			process_array_accessors(tokens, tokens.begin());
 			tokens = tokenize(build_string(tokens));
 			process_new_keyword(tokens, tokens.begin());
 			tokens = tokenize(build_string(tokens));
 			process_del_keyword(tokens, tokens.begin());
 
-			output = build_string(tokens);
-
-			return output;
+			return build_string(tokens);
+			
 		}
 	}
 }
--- src/compiler/tokenizer.hpp
+++ src/compiler/tokenizer.hpp
@@ -20,7 +20,8 @@ namespace carma {
 			ENDOFSTATEMENT,
 			CODEBLOCK,
 			ARRAY,
-			EMPTY
+			EMPTY,
+			METHODCALL
 		};
 	};
 
