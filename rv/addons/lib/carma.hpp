ac97b8a5fe41e2578a2eb862fe8c147af88ecea3
Nou <cliff.foster@idi-systems.com>
2015-11-15 21:31:06 -0800

    Add critical section helpers.

--- rv/addons/lib/carma.hpp
+++ rv/addons/lib/carma.hpp
@@ -5,3 +5,6 @@
 
 #define CRITICAL_PARAMS   params ["_counter"]; (carma2_criticalArgs select _counter) params 
 #define CRITICAL_SETRETURN(val) carma2_criticalArgs set[_counter, val]
+
+#define carma2_start_crit_section ____carma_crit_section = {
+#define carma2_end_crit_section }; "_this call ____carma_crit_section" configClasses (configfile >> "CarmaBlank")
